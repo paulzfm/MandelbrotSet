d4ec98f7e2e8333d186b123885f3ecee68e52a8e
paulzfm <zhufengminpaul@163.com>
2015-07-13 10:18:08 +0800

    add test

--- test.c
+++ test.c
@@ -8,6 +8,8 @@ int main()
     #pragma omp parallel for private(j)
     for (i = 0; i < 10; i++) {
         for (j = 0; j < 10; j++) {
+            int k;
+            for (k = 0; k < 10000; k++);
             printf("(%d, %d)\n", i, j);
         }
     }
