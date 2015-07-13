06005c9236e3bc828ed549466c8aa1f55af1f114
paulzfm <zhufengminpaul@163.com>
2015-07-13 10:24:56 +0800

    add test

--- test.c
+++ test.c
@@ -4,6 +4,7 @@
 int main()
 {
     int i, j;
+    int cnt = 0;
 
     #pragma omp parallel for private(j)
     for (i = 0; i < 400; i++) {
@@ -11,8 +12,11 @@ int main()
             int k;
             for (k = 0; k < 10000; k++);
             printf("(%d, %d)\n", i, j);
+            #pragma omp critical
+            cnt++;
         }
     }
 
+    printf("%d\n", cnt);
     return 0;
 }
